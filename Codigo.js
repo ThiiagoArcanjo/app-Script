c26a7e69ef1ac517f325cb2ae8c653682f8fb270
unknown <thiagokai.arcanjo@gmail.com>
2022-03-14 12:57:35 -0300

    Ultimas alterações

--- Codigo.js
+++ Codigo.js
@@ -1,4 +1,114 @@
+function validarTipoDeProgressao(habilitado, tipo, capacitacao){
+  if(habilitado == "Habilitado"){
+    if(tipo == "Horizontal e Vertical"){
+      if(capacitacao != ""){
+        return "Valido para Horizontal e Vertical";
+      
+      }else if(capacitacao == ""){
+        return "Valido para Horizontal";
+      }
+    } else if(tipo == "Vertical"){
+      
+       if(capacitacao != ""){
+        return "Valido para Vertical";
+      }
+
+    }else{
+      return "Valido para Horizontal";
+    }
+  }else {
+    return "Não Habilitado";
+  }
+
+  
+}
+
 function tratarDados(
+  capacitacao,
+  matricula,
+  nota,
+  tempoDeCasa,
+  tipoDeProgressao,
+  numeroDeFilhos,
+  faltas,
+  atrasos
+) {
+
+  let params = {}
+
+  params.matricula = String(matricula);
+  params.nota = Number(nota);
+  params.tempoDeCasa = String(tempoDeCasa);
+  params.capacitacao = String(capacitacao);
+  params.tipoDeProgressao = String(tipoDeProgressao);
+  params.numeroDeFilhos = Number(numeroDeFilhos);
+  params.faltas = Number(faltas);
+  params.atrasos = Number(atrasos);
+
+  Logger.log(params);
+  return params;
+}
+
+
+function validacaoCapacitacao(
+  params = { tipoDeProgressao: '', capacitacao: '' }
+) {
+
+  if (params.tipoDeProgressao == "Vertical") {
+    if (params.capacitacao != "") {
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function diasTrabalhados(dtInicioString) {
+
+  try {
+    /* Separa os valores com / */
+    
+    var dataString = dtInicioString.split("/");
+
+    var dtInicio = new Date(dataString[2], dataString[1] - 1, dataString[0]);// Menos -1 posto para ajustar o mes no js
+
+    var tempoAtual = new Date(); // Pega datatime
+    var dataDiferenca = tempoAtual.getTime() - dtInicio.getTime();
+    var tempoDeTrabalho = Math.trunc(dataDiferenca / (24 * 60 * 60 * 1000));
+
+    return tempoDeTrabalho
+  } catch (error) {
+    
+    return false;
+  }
+
+
+}
+
+function validarAtrasos(qtdAtrasos){
+  var atrasos = Number(qtdAtrasos);
+  if(atrasos > 20){
+    return false;
+  }
+  return true;
+}
+
+function validarFaltas(qtdFaltas){
+  var faltas = Number(qtdFaltas);
+  if(faltas > 18){
+    return false;
+  }
+  return true;
+}
+
+function validacaoParaProgressao(
+  matricula, nota, tempoDeCasa, tipoDeProgressao, capacitacao, numeroDeFilhos = 0, faltas, atrasos
+) {
+
+  const dados = tratarDados(
     capacitacao,
     matricula,
     nota,
@@ -7,114 +117,26 @@ function tratarDados(
     numeroDeFilhos,
     faltas,
     atrasos
-  ) {
+    )
+
+  if (validacaoCapacitacao({ tipoDeProgressao: dados.tipoDeProgressao, capacitacao: dados.capacitacao }) == false) {
+    return 'Não habilitado, não inseriu capacitação';
+  }
+
+  var diasDeTrabalho = diasTrabalhados(dados.tempoDeCasa); // Alterar quando tivermos as novas planilhas
   
-    let params = {}
-  
-    params.matricula = String(matricula);
-    params.nota = Number(nota);
-    params.tempoDeCasa = String(tempoDeCasa);
-    params.capacitacao = String(capacitacao);
-    params.tipoDeProgressao = String(tipoDeProgressao);
-    params.numeroDeFilhos = Number(numeroDeFilhos);
-    params.faltas = Number(faltas);
-    params.atrasos = Number(atrasos);
-  
-    Logger.log(params);
-    return params;
+  if(diasDeTrabalho < 1095){
+    return 'Não habilitado, possui menos de 3 anos de trabalho';
   }
   
-  
-  function validacaoCapacitacao(
-    params = { tipoDeProgressao: '', capacitacao: '' }
-  ) {
-  
-    if (params.tipoDeProgressao == "Vertical" || params.tipoDeProgressao == "Horizontal e Vertical") {
-      if (params.capacitacao != "") {
-        return true;
-  
-      } else {
-        return false;
-      }
-    }
-  
-    return true;
+  if(validarAtrasos(dados.atrasos) === false){
+    return 'Não habilitado, possui mais de 20 atrasos';
   }
-  
-  function diasTrabalhados(dtInicioString) {
-  
-    try {
-      /* Separa os valores com / */
-      
-      var dataString = dtInicioString.split("/");
-  
-      var dtInicio = new Date(dataString[2], dataString[1] - 1, dataString[0]);// Menos -1 posto para ajustar o mes no js
-  
-      var tempoAtual = new Date(); // Pega datatime
-      var dataDiferenca = tempoAtual.getTime() - dtInicio.getTime();
-      var tempoDeTrabalho = Math.trunc(dataDiferenca / (24 * 60 * 60 * 1000));
-  
-      return tempoDeTrabalho
-    } catch (error) {
-      
-      return false;
-    }
-  
-  
+
+  if(validarFaltas(dados.faltas) === false){
+    return 'Não habilitado, possui mais de 18 faltas';
   }
-  
-  function validarAtrasos(qtdAtrasos){
-    var atrasos = Number(qtdAtrasos);
-    if(atrasos > 20){
-      return false;
-    }
-    return true;
-  }
-  
-  function validarFaltas(qtdFaltas){
-    var faltas = Number(qtdFaltas);
-    if(faltas > 18){
-      return false;
-    }
-    return true;
-  }
-  
-  function validacaoParaProgressao(
-    matricula, nota, tempoDeCasa, tipoDeProgressao, capacitacao, numeroDeFilhos = 0, faltas, atrasos
-  ) {
-  
-    const dados = tratarDados(
-      capacitacao,
-      matricula,
-      nota,
-      tempoDeCasa,
-      tipoDeProgressao,
-      numeroDeFilhos,
-      faltas,
-      atrasos
-      )
-  
-    if (validacaoCapacitacao({ tipoDeProgressao: dados.tipoDeProgressao, capacitacao: dados.capacitacao }) == false) {
-      return false;
-    }
-  
-    diasTrabalhos = diasTrabalhados(dados.tempoDeCasa); // Alterar quando tivermos as novas planilhas
-    
-    if(diasTrabalhos < 1095){
-      return false;
-    }
-    
-    if(validarAtrasos(dados.atrasos) === false){
-      return false;
-    }
-  
-    if(validarFaltas(dados.faltas) === false){
-      return false;
-    }
-  
-  
-    return true;
-  }
-  
-  
-  //console.log(validacaoParaProgressao(7288, 70, '02/02/2017', 'Horizontal', 'asf', 2, 1, 1));
+
+
+  return 'Habilitado';
+}
--- compararPlanilhas.js
+++ compararPlanilhas.js
@@ -1,147 +1,143 @@
 function ajustarPlanilhas() {
-    // DADOS DO REQUERIMENTO
-    var requerimentoDeProgressaoFormulario = {
-      id: '1k9Kl-GPKGasxXv2yqt5q-Ypbyi_dY45T1hEleThPhZE',
-      sheetName: 'dados',
-      data: null
-    }
-  
-    requerimentoDeProgressaoFormulario.data = pegaPlanilha(
-      requerimentoDeProgressaoFormulario.id, requerimentoDeProgressaoFormulario.sheetName
-    );
-  
-    // DADOS DA AVALIAÇÃO DO RH
-    var dadosRHNotaAvaliacao = {
-      id: '13ZXkWFZ6ESTAyNI26LIlcEHK00KRhvcpA3MHYEjDdC4',
-      sheetName: 'nota-avaliacao',
-      data: null
-    }
-  
-    dadosRHNotaAvaliacao.data = pegaPlanilha(
-      dadosRHNotaAvaliacao.id, dadosRHNotaAvaliacao.sheetName
-    );
-  
-  
-    // DADOS DOS FUNCIONARIOS DO RH
-    var dadosRhDIFuncionarios = {
-      id: '13ZXkWFZ6ESTAyNI26LIlcEHK00KRhvcpA3MHYEjDdC4',
-      sheetName: 'dados-funcionario',
-      data: null
-    }
-  
-    dadosRhDIFuncionarios.data = pegaPlanilha(
-      dadosRhDIFuncionarios.id, dadosRhDIFuncionarios.sheetName
-    );
-  
-  
-    // DADOS Lecencas e afastamentos do RH 
-    var dadosRhLicencasAfastamentos = {
-      id: '13ZXkWFZ6ESTAyNI26LIlcEHK00KRhvcpA3MHYEjDdC4',
-      sheetName: 'licencas-afastamentos',
-      data: null
-    }
-  
-    dadosRhLicencasAfastamentos.data = pegaPlanilha(
-      dadosRhLicencasAfastamentos.id, dadosRhLicencasAfastamentos.sheetName
-    );
-  
-    // COMPARAR LINHAS PARA ENCONTRAR MATRICULA DO FUNCIONARIO
-    // for index no 1 para tirar o cabeçalho e convertemos o logo em string para evitar ,00
-    // For para ler toda planilha do forms
-    for (var i = 1; i <= requerimentoDeProgressaoFormulario.data.numRows - 1; i++) {
-      var row = requerimentoDeProgressaoFormulario.data.values[i];
-      var matriculaRequerimento = String(row[6]);
-  
-      //For para ler a planilha de avaliação dos funcionarios
-      for (var j = 1; j <= dadosRHNotaAvaliacao.data.numRows - 1; j++) {
-  
-        var rowDadoAvaliacao = dadosRHNotaAvaliacao.data.values[j];
-        var matriculaDadoAvaliacao = String(rowDadoAvaliacao[3]);
-        var ultimaNotaDadoAvaliacao = String(rowDadoAvaliacao[2]);
-  
-        if (matriculaDadoAvaliacao == matriculaRequerimento) {
-          if (ultimaNotaDadoAvaliacao.trim() != '' && ultimaNotaDadoAvaliacao != null) {
-            SpreadsheetApp.getActiveSheet().getRange(("M" + (i + 1))).setValue(String(ultimaNotaDadoAvaliacao));
-          }
-        }
-      }
-  
-  
-      // For de informações dos funcioarios // pega data nascimento e da inicio do trablho
-      for (var h = 1; h <= dadosRhDIFuncionarios.data.numRows - 1; h++) {
-        var rowMatricula = dadosRhDIFuncionarios.data.values[h];
-        var colMatricula = String(rowMatricula[1]);
-        var colDtNascimento = String(rowMatricula[2]);
-        var colDtEntrada = String(rowMatricula[3]);
-  
-  
-  
-        if (matriculaRequerimento == colMatricula) {
-          if (colDtNascimento.trim() != '' && colDtNascimento != null) {
-            SpreadsheetApp.getActiveSheet().getRange(("F" + (i + 1))).setValue(String(colDtNascimento));
-          }
-  
-          if (colDtEntrada.trim() != '' && colDtEntrada != null) {
-            SpreadsheetApp.getActiveSheet().getRange(("Q" + (i + 1))).setValue(String(colDtEntrada));
-          }
-  
-        }
-      }
-  
-  
-  
-      // for para pegar os numeros de licenças
-      for (var x = 1; x <= dadosRhLicencasAfastamentos.data.numRows - 1; x++) {
-        var licencaRow = dadosRhLicencasAfastamentos.data.values[x];
-  
-        //pega matriccula da segunda planinha
-        var licencaMatricula = String(licencaRow[0]);
-                 
-        //pega o atestado que esta na segunda planilha
-        var diasAtestado = Number(licencaRow[3]);
-        // Compara matricula da primeira planinha com o da segunda
-        if (matriculaRequerimento == licencaMatricula) {
-          Logger.log("Log primeiro if");
-          Logger.log(licencaMatricula);
-         if (diasAtestado != null && diasAtestado > 0) {
-            var soma = Number(SpreadsheetApp.getActiveSheet().getRange(("P" + (i + 1))).getValue())
-              + Number(diasAtestado)
-  
-            Logger.log(soma);
-            Logger.log("Log if");
-            SpreadsheetApp.getActiveSheet().getRange(("P" + (i + 1))).setValue(String(soma));
-          } 
-        }
-      }
-  
-  
-  
-  
-  
-    }
-  
+  // DADOS DO REQUERIMENTO
+  var requerimentoDeProgressaoFormulario = {
+    id: '1lWMD0rxbLQJ7H18jM-vVSfxlTaSTSiM9VVZK3tYyd74',
+    sheetName: 'Respostas ao formulário 1',
+    data: null
   }
-  
-  
-  
-  function pegaPlanilha(id, sheetName) {
-    Logger.log('pegaPlanilha');
-    Logger.log(id);
-    Logger.log(sheetName);
-    var spreadsheet = SpreadsheetApp.openById(id);
-    var sheet = spreadsheet.getSheetByName(sheetName);
-    var rows = sheet.getDataRange();
-    var numRows = rows.getNumRows();
-    var values = rows.getValues();
-    return {
-      values: values,
-      numRows: numRows
-    };
-    /*
-    for (var i = 0; i <= numRows - 1; i++) {
-      var row = values[i];
-      Logger.log(row[6]);
-    }
-    */
+
+  requerimentoDeProgressaoFormulario.data = pegaPlanilha(
+    requerimentoDeProgressaoFormulario.id, requerimentoDeProgressaoFormulario.sheetName
+  );
+
+  // DADOS DA AVALIAÇÃO DO RH
+  var dadosRHNotaAvaliacao = {
+    id: '13ZXkWFZ6ESTAyNI26LIlcEHK00KRhvcpA3MHYEjDdC4',
+    sheetName: 'nota-avaliacao',
+    data: null
   }
-  
+
+  dadosRHNotaAvaliacao.data = pegaPlanilha(
+    dadosRHNotaAvaliacao.id, dadosRHNotaAvaliacao.sheetName
+  );
+
+
+  // DADOS DOS FUNCIONARIOS DO RH
+  var dadosRhDIFuncionarios = {
+    id: '13ZXkWFZ6ESTAyNI26LIlcEHK00KRhvcpA3MHYEjDdC4',
+    sheetName: 'licencas-dados-funcionarios',
+    data: null
+  }
+
+  dadosRhDIFuncionarios.data = pegaPlanilha(
+    dadosRhDIFuncionarios.id, dadosRhDIFuncionarios.sheetName
+  );
+
+
+  // DADOS Lecencas e afastamentos do RH 
+  var dadosRhLicencasAfastamentos = {
+    id: '13ZXkWFZ6ESTAyNI26LIlcEHK00KRhvcpA3MHYEjDdC4',
+    sheetName: 'licenca-afastamento-fazenda',
+    data: null
+  }
+
+  dadosRhLicencasAfastamentos.data = pegaPlanilha(
+    dadosRhLicencasAfastamentos.id, dadosRhLicencasAfastamentos.sheetName
+  );
+
+  // COMPARAR LINHAS PARA ENCONTRAR MATRICULA DO FUNCIONARIO
+  // for index no 1 para tirar o cabeçalho e convertemos o logo em string para evitar ,00
+  // For para ler toda planilha do forms
+  for (var i = 1; i <= requerimentoDeProgressaoFormulario.data.numRows - 1; i++) {
+    var row = requerimentoDeProgressaoFormulario.data.values[i];
+    var matriculaRequerimento = String(row[7]);
+
+    //For para ler a planilha de avaliação dos funcionarios
+    for (var j = 1; j <= dadosRHNotaAvaliacao.data.numRows - 1; j++) {
+
+      var rowDadoAvaliacao = dadosRHNotaAvaliacao.data.values[j];
+      var matriculaDadoAvaliacao = String(rowDadoAvaliacao[3]);
+      var ultimaNotaDadoAvaliacao = String(rowDadoAvaliacao[2]);
+
+      if (matriculaDadoAvaliacao == matriculaRequerimento) {
+        if (ultimaNotaDadoAvaliacao.trim() != '' && ultimaNotaDadoAvaliacao != null) {
+          SpreadsheetApp.getActiveSheet().getRange(("N" + (i + 1))).setValue(String(ultimaNotaDadoAvaliacao));
+        }
+      }
+    }
+
+
+    // For de informações dos funcioarios // pega data nascimento e da inicio do trablho
+    for (var h = 1; h <= dadosRhDIFuncionarios.data.numRows - 1; h++) {
+      var rowMatricula = dadosRhDIFuncionarios.data.values[h];
+      var colMatricula = String(rowMatricula[1]);
+      var colDtNascimento = String(rowMatricula[2]);
+      var colDtEntrada = String(rowMatricula[3]);
+
+
+
+      if (matriculaRequerimento == colMatricula) {
+        if (colDtNascimento.trim() != '' && colDtNascimento != null) {
+          SpreadsheetApp.getActiveSheet().getRange(("G" + (i + 1))).setValue(String(colDtNascimento));
+          SpreadsheetApp.getActiveSheet().getRange(("R" + (i + 1))).setValue(String(colDtEntrada));
+        }
+
+      }
+    }
+
+
+
+    // for para pegar os numeros de licenças
+    for (var x = 1; x <= dadosRhLicencasAfastamentos.data.numRows - 1; x++) {
+      var licencaRow = dadosRhLicencasAfastamentos.data.values[x];
+
+      //pega matriccula da segunda planinha
+      var licencaMatricula = String(licencaRow[0]);
+               
+      //pega o atestado que esta na segunda planilha
+      var diasAtestado = Number(licencaRow[3]);
+      // Compara matricula da primeira planinha com o da segunda
+      if (matriculaRequerimento == licencaMatricula) {
+        Logger.log("Log primeiro if");
+        Logger.log(licencaMatricula);
+       if (diasAtestado != null && diasAtestado > 0) {
+          var soma = Number(SpreadsheetApp.getActiveSheet().getRange(("Q" + (i + 1))).getValue())
+            + Number(diasAtestado)
+
+          Logger.log(soma);
+          Logger.log("Log if");
+          SpreadsheetApp.getActiveSheet().getRange(("Q" + (i + 1))).setValue(String(soma));
+        } 
+      }
+    }
+
+
+
+
+
+  }
+
+}
+
+
+
+function pegaPlanilha(id, sheetName) {
+  Logger.log('pegaPlanilha');
+  Logger.log(id);
+  Logger.log(sheetName);
+  var spreadsheet = SpreadsheetApp.openById(id);
+  var sheet = spreadsheet.getSheetByName(sheetName);
+  var rows = sheet.getDataRange();
+  var numRows = rows.getNumRows();
+  var values = rows.getValues();
+  return {
+    values: values,
+    numRows: numRows
+  };
+  /*
+  for (var i = 0; i <= numRows - 1; i++) {
+    var row = values[i];
+    Logger.log(row[6]);
+  }
+  */
+}
